f1da25e3610bcf709fff562d710e915c54bd7a6a
humuraelvin <humura53@gmail.com>
2024-09-12 07:59:41 +0200

    first commit

---
+++ resources/views/notes/index.blade.php
@@ -0,0 +1,13 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Notes') }}
+        </h2>
+    </x-slot>
+
+
+    <div class="max-w-2xl mx-auto sm:px-6 lg:px-8 p-4">
+        <!-- #region -->
+    </div>
+
+</x-app-layout>
