37312602428f92cec1f91a269f7713583a432c69
humuraelvin <humura53@gmail.com>
2024-09-13 06:21:32 +0200

    changes added

---
+++ resources/views/components/red-btn-link.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-red-600 dark:bg-rose-500 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-red-700 dark:hover:bg-rose-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</a>
