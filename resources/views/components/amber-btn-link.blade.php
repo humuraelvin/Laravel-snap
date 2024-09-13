19981c2752c80357f274639b503b075127ad0af2
humuraelvin <humura53@gmail.com>
2024-09-13 06:24:04 +0200

    changes added

--- resources/views/components/amber-btn-link.blade.php
+++ resources/views/components/amber-btn-link.blade.php
@@ -1,4 +1,3 @@
-// resources/views/components/amber-btn-link.blade.php
-<a {{ $attributes->merge(['class' => 'btn amber-btn']) }}>
+<a {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-amber-500 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-green-700 dark:hover:bg-amber-300 focus:bg-green-700 dark:focus:bg-white active:bg-green-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </a>
